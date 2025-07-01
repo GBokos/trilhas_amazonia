6943355400a0a5753a83b35de78c811ea51270d3
GBokos <57645672+GBokos@users.noreply.github.com>
2025-07-01 01:59:48 +0000

    Adicao .github e load secrets

--- restore_api2.sh
+++ restore_api2.sh
@@ -1,9 +1,9 @@
 #!/bin/bass
 
 # Restaura o .env da API2
-echo "$API2_ENV" > ./trilhasAmazonia/.env
+echo "$API2_ENV" > ./trilhasAmazonia2/.env
 
 # Restaura o arquivo de credenciais JSON
-echo "$API2_CRED" > ./trilhasAmazonia/credenciais_api.json
+echo "$API2_CRED" > ./trilhasAmazonia2/credenciais_api.json
 
 echo "✅ Arquivos restaurados em ./trilhasAmazonia2"
